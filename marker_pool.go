309fe371d3ec0d27d769e0d3ddb4c52b414e2dff
hayabusa-cloud <git@hybscloud.com>
2026-02-23 22:07:51 +0900

    feat: add pooled genericMarker in advance

---
+++ marker_pool.go
@@ -0,0 +1,33 @@
+// ©Hayabusa Cloud Co., Ltd. 2026. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+package kont
+
+import "sync"
+
+var genericMarkerPool = sync.Pool{
+	New: func() any { return new(genericMarker) },
+}
+
+type genericMarker struct {
+	op     Operation
+	resume func(*genericMarker, Resumed) Resumed
+	f      any
+	k      any
+}
+
+func (m *genericMarker) Op() Operation            { return m.op }
+func (m *genericMarker) Resume(v Resumed) Resumed { return m.resume(m, v) }
+
+func acquireMarker() *genericMarker {
+	return genericMarkerPool.Get().(*genericMarker)
+}
+
+func releaseMarker(m *genericMarker) {
+	m.op = nil
+	m.resume = nil
+	m.f = nil
+	m.k = nil
+	genericMarkerPool.Put(m)
+}
